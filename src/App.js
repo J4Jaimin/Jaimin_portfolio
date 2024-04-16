5631ebad6278f20db63a034b0e1b56df861bfa7d
Jaimin Rana <jaiminrana1102@gmail.com>
2024-04-16 18:22:25 +0530

    add contact section and styled it

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Home from './components/Home';
 import Work from './components/Work';
 import TimeLine from './components/TimeLine';
 import Services from './components/Services';
+import { Toaster } from 'react-hot-toast';
 
 import './styles/app.scss';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Work />
       <TimeLine />
       <Services />
+      <Contact />
+      <Toaster />
     </>
   );
 }
