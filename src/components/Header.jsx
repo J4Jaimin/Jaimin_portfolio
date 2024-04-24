2ec6c41e0e84ae0ec22bdf861dcfdefcd9b5896e
Jaimin-1102 <jaimin.rana@agshealth.com-at-205008539362>
2024-04-24 08:27:21 +0530

    added new headerPhone component and styled it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 
-const Header = () => {
+const Header = ({ menuOpen, setMenuOpen }) => {
     return (
-        <nav>
-            <NavContent />
-            <button>
+        <>
+            <nav>
+                <NavContent setMenuOpen={setMenuOpen} />
+            </nav>
+
+            <button className='navBtn' onClick={() => setMenuOpen(!menuOpen)}>
                 <AiOutlineMenu />
             </button>
-        </nav>
+        </>
     )
 }
 
-const NavContent = () => (
+export const HeaderPhone = ({ menuOpen, setMenuOpen }) => {
+    return (
+        <div className={`navPhone ${menuOpen ? "navPhoneComes" : ""}`}>
+            <NavContent setMenuOpen={setMenuOpen} />
+        </div>
+    )
+}
+
+const NavContent = ({ setMenuOpen }) => (
     <>
         <h2>Jaimin.</h2>
         <div>
-            <a href='#home'>Home</a>
-            <a href='#work'>work</a>
-            <a href='#timeline'>Timeline</a>
-            <a href='#services'>Services</a>
-            <a href='#contact'>Contact</a>
+            <a onClick={() => setMenuOpen(false)} href='#home'>Home</a>
+            <a onClick={() => setMenuOpen(false)} href='#work'>work</a>
+            <a onClick={() => setMenuOpen(false)} href='#timeline'>Timeline</a>
+            <a onClick={() => setMenuOpen(false)} href='#services'>Services</a>
+            <a onClick={() => setMenuOpen(false)} href='#contact'>Contact</a>
         </div>
         <a href='mailto: jaiminrana1102@gmail.com'>
             <button>Email</button>
